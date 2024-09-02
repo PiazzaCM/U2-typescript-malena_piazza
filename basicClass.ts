3c903d7a327e1f0c457eb90a6b6f9e4b7f00dc46
Piazza <carlamalenapiazza@gmail.com>
2024-09-02 10:36:27 -0300

    punto 7

---
+++ basicClass.ts
@@ -0,0 +1,16 @@
+//Define una clase Car que tenga una propiedad make (marca del coche) de tipo string 
+//y un método drive() que imprima un mensaje en la consola diciendo "Driving a [make]". 
+
+class Car {
+    make : string;
+
+    constructor(make : string) {
+        this.make = make;
+    }
+
+    drive() {
+        console.log('Driving a ' + this.make);
+    }
+}
+
+let car = new Car('toyota');
