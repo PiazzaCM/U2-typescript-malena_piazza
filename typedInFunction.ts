96591ca8b4f1bed64c0cc6803f23d29b9913a02f
Piazza <carlamalenapiazza@gmail.com>
2024-09-02 10:32:35 -0300

    punto 6

---
+++ typedInFunction.ts
@@ -0,0 +1,7 @@
+//Crea una función greet que tome un argumento name de tipo string y devuelva una frase de saludo usando ese nombre.
+
+function greet(username : string) {
+    return 'ayer vino ' + username;
+}
+
+console.log(greet('juan')); // ayer vino juan
