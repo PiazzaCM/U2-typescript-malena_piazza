c534ac3d3a5fc8a6a454beead95ab2fddb3ee614
Piazza <carlamalenapiazza@gmail.com>
2024-09-02 10:12:30 -0300

    punto 1 y 2

---
+++ basicTypes.ts
@@ -0,0 +1,7 @@
+//Declara una variable age de tipo número y asígnale un valor.
+let age : number = 25;
+
+//Declara una variable name de tipo string y asígnale un valor.
+//name es una palabra reservada en TypeScript, por lo que no se puede usar como nombre de variable.
+let username : string = "juan";
+
---
+++ unionTypes.ts
@@ -0,0 +1,7 @@
+//Declara una variable status que pueda ser de tipo string o número. Asígnale diferentes valores para probar ambos tipos.
+//status es una palabra reservada en TypeScript, por lo que no se puede usar como nombre de variable.
+let estado : number | string = 200;
+
+let estado2 : number | string = "OK";
+
+console.log(estado);
