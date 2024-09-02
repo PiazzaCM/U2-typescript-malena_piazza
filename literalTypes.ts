fd70b84a8964b7cc14a7e997011c2a92712d986d
Piazza <carlamalenapiazza@gmail.com>
2024-09-02 12:48:06 -0300

    punto 12

---
+++ literalTypes.ts
@@ -0,0 +1,7 @@
+//Crea una variable direction que solo pueda tener uno de los siguientes valores: "up", "down", "left" o "right".
+
+let direccion : "up" | "down" | "left" | "right";
+
+direccion = "up";
+
+console.log(direccion); // up
